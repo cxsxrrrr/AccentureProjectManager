8981fdd699d30ee0e371a08463128b0297a0bc83
SamuelRoVe06 <165357269+SamuelRoVe06@users.noreply.github.com>
2025-06-05 17:05:31 -0400

    diseño de pantalla de login con Tailwind
    - Implementado diseño responsive del login usando tailwind
    - Dividido Pagina en dos paneles: logo izquierdo y formulario derecho
    - Añadido color personalizado accenture (#A100FF)
    - Logo centrado y Copyright posicionado en esquina inferior izquierda
    - Formulario de login con inputs, boton y validaciones
    - Estructura de carpetas organizada en `components` y `pages`

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './pages/Login';
-// import App from './App'
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
-    {/* <App/> */}
   </StrictMode>,
 )
