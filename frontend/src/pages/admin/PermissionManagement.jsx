76d00b3333b84b7ccb4c5f8165fa6e5573e3a34f
Luis <luisibp0410@gmail.com>
2025-06-05 22:43:48 -0400

    Commit 2: Modulo Admin
    Estructura del modulo de Admin terminada, falta contenido de las listas y dahsboard.

--- frontend/src/pages/admin/PermissionManagement.jsx
+++ frontend/src/pages/admin/PermissionManagement.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Topbar from "../../components/admin/layout/Topbar";
+import TopControls from "../../components/admin/layout/TopControls";
+import "../../stylesheets/page.css";
 
 function PermissionManagement() {
-  return <h1>PermissionManagement</h1>;
+  return (
+    <div className="admin-page">
+      <Topbar title="Permission Management">
+        <TopControls module="permission" />
+      </Topbar>
+      <div className="admin-content">
+        <table>
+          <thead><tr><th>Permission</th><th>Description</th></tr></thead>
+          <tbody><tr><td>Edit</td><td>Allows editing</td></tr></tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default PermissionManagement;
--- frontend/src/pages/admin/RoleManagement.jsx
+++ frontend/src/pages/admin/RoleManagement.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Topbar from "../../components/admin/layout/Topbar";
+import TopControls from "../../components/admin/layout/TopControls";
+import "../../stylesheets/page.css";
 
 function RoleManagement() {
-  return <h1>RoleManagement</h1>;
+  return (
+    <div className="admin-page">
+      <Topbar title="Role Management">
+        <TopControls module="role" />
+      </Topbar>
+      <div className="admin-content">
+        <table>
+          <thead><tr><th>Role</th><th>Description</th></tr></thead>
+          <tbody><tr><td>Admin</td><td>Full access</td></tr></tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default RoleManagement;
