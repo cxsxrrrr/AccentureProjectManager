a1f4bdb8dbb01586a4e2b246905d872075f06e15
Claudio <jojosoludos2004@gmail.com>
2025-06-27 19:00:03 -0400

    Recursos (xfavo no te borres papadio)

---
+++ backend/src/main/java/com/Accenture/backend/dao/RecursoDAOImpl.java
@@ -0,0 +1,69 @@
+package com.Accenture.backend.dao;
+
+import com.Accenture.backend.model.Recurso;
+import com.Accenture.backend.domain.repository.RecursoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class RecursoDAOImpl implements RecursoDAO {
+    private final RecursoRepository recursoRepository;
+
+    // Constructor básico
+    public RecursoDAOImpl(RecursoRepository recursoRepository) {
+        this.recursoRepository = recursoRepository;
+    }
+
+
+    // Crea el recurso en la Base De Datos
+    @Transactional
+    @Override
+    public Recurso guardarRecurso(Recurso recurso) {
+        // REPOSITORIO DE FUNCIONES STANDARD
+        return recursoRepository.save(recurso);
+    }
+
+    // Guarda el recurso actualizado en la base de datos
+    @Transactional
+    @Override
+    public Recurso actualizarRecurso(Recurso recurso) {
+        return recursoRepository.save(recurso);
+    }
+
+    // Elimina el recurso de la base de datos
+    @Transactional
+    @Override
+    public void eliminarRecurso(Recurso recurso) {
+        recursoRepository.delete(recurso);
+    }
+
+    // Busca el recurso en la base de datos x ID
+    @Override
+    public Recurso buscarRecursoxID(Long idRecurso) {
+        return recursoRepository.findById(idRecurso).orElse(null);
+    }
+
+    // Busca los recursos por nombres
+    @Override
+    public List<Recurso> buscarRecursosPorNombre(String nombre) {
+        return proyectoRepository.findByNombreProyectoContainingIgnoreCase(nombre);
+    }
+
+    // Busca los recursos por estado
+    @Override
+    public List<Recurso> buscarRecursosPorEstado(String estado) {
+        if (estado == null || estado.isEmpty()) {
+            throw new IllegalArgumentException("El estado no puede ser nulo o vacío");
+        }
+        return recursoRepository.findByEstadoIgnoreCase(estado);
+    }
+
+
+    // Obtener una lista con los recursos
+    @Override
+    public List<Recurso> obtenerRecurso() {
+        return recursoRepository.findAll();
+    }
+}
---
+++ backend/src/main/java/com/Accenture/backend/domain/dto/RecursoDTO.java
@@ -0,0 +1,27 @@
+package com.Accenture.backend.domain.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+// Datos visibles en la petición
+public class RecursoDTO {
+
+    private Long recursoId;
+    private String nombreRecurso;
+    private String descripcionRecurso;
+    private String estado;
+    private String coste;
+    private String tipo;
+    private UsuarioDTO gerenteProyecto;
+    private UsuarioDTO creadoPor;
+    private LocalDateTime fechaCreacion;
+}
