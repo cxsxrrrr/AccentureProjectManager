f1111d98acf779bef75999066514f6c09114320d
Enma0101 <enmanuel.aft@gmail.com>
2025-06-27 04:54:13 -0400

    act 50%

---
+++ frontend/src/pages/projectManager/DocumentsManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocumentsManagement = () => {
+  return (
+    <div>
+      <h1>Gestión de Documentos</h1>
+    </div>
+  );
+};
+
+export default DocumentsManagement;
