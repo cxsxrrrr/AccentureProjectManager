b3898be64f1681b0b650372036abc4b8ac77a79f
SamuelRoVe06 <165357269+SamuelRoVe06@users.noreply.github.com>
2025-06-28 01:11:41 -0400

    Update ProjectStatusTable.jsx

--- frontend/src/components/dashboard/ProjectStatusTable.jsx
+++ frontend/src/components/dashboard/ProjectStatusTable.jsx
@@ -9,7 +9,7 @@ const ProjectStatusTable = ({ data = [] }) => (
       <thead>
         <tr>
           <th className="text-left px-6 py-3 font-semibold">Task Name</th>
-          <th className="text-left px-6 py-3 font-semibold">Department</th>
+          <th className="text-left px-6 py-3 font-semibold">Category</th>
           <th className="text-left px-6 py-3 font-semibold">Status</th>
         </tr>
       </thead>
@@ -18,16 +18,15 @@ const ProjectStatusTable = ({ data = [] }) => (
           data.map((e, i) => (
             <tr key={i} className="border-t">
               <td className="px-6 py-4">{e.task}</td>
-              <td className="px-6 py-4">{e.dept}</td>
-
+              <td className="px-6 py-4">{e.category}</td>
               <td className="px-6 py-4">
                 <span
                   className={`px-4 py-2 rounded-full text-base font-bold
                     ${
-                      e.status === "Permanent"
+                      e.status === "Active"
                         ? "bg-green-100 text-green-700"
-                        : e.status === "Contract"
-                        ? "bg-gray-200 text-gray-800"
+                        : e.status === "Inactive"
+                        ? "bg-red-100 text-red-600"
                         : "bg-yellow-100 text-yellow-600"
                     }`}
                 >
