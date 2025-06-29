bfdcaf4c76ac327651516f56269369ac996b3e50
Luis Solarte <luisibp0410@gmail.com>
2025-06-29 01:25:46 -0400

    Submodulo Client terminado

--- frontend/src/pages/principals/AdminLayout.jsx
+++ frontend/src/pages/principals/AdminLayout.jsx
@@ -3,12 +3,11 @@ import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/common/Sidebar"; 
 import adminMenu from "../../config/sidebars/AdminMenu";
 import accentureLogo from "../../assets/accenture-logo.svg";
+
 function AdminLayout() {
   const location = useLocation();
   const path = location.pathname;
-
   const currentMenuItem = adminMenu.find(item => path.startsWith(item.path));
-
   const vista = currentMenuItem?.path || "/admin/dashboard";
   const Component = currentMenuItem?.component || adminMenu[0].component;
 
